bff806fdbe2355881d7415429acf3fcc375f3e38
nad12 <mgardasaib@umail.utm.ac.mu>
2019-09-03 15:08:52 +0400

    30k + record under 5 seconds import

--- src/Command/CsvImportCommand.php
+++ src/Command/CsvImportCommand.php
@@ -34,18 +34,21 @@ class CsvImportCommand extends ContainerAwareCommand
     {
         $em = $this->getEntityManager();
         $em->getConnection()->getConfiguration()->setSQLLogger(null);
-        $path = $this->getContainer()->get('kernel')->getProjectDir() . '\public\\';
-        $reader = Reader::createFromPath($path . '\text.csv');
-        $reader->setHeaderOffset(0);
+        $path = $this->getContainer()->get('kernel')->getProjectDir() . '\public\text.csv';
+//        $reader = Reader::createFromPath($path . '\text.csv');
+//        $reader->setHeaderOffset(0);
 
-        foreach ($reader as $r) {
-            $sql = <<<SQL
-              INSERT INTO `test_entity` (`eq_site_limit`, `hu_site_limit`) VALUES (:eq_limit, :hu_limit);
-SQL;
+//        foreach ($reader as $r) {
+            $sql =
+            sprintf("LOAD DATA INFILE '%s' INTO TABLE test_entity
+                        FIELDS TERMINATED BY ','
+                        LINES TERMINATED BY '\\n'
+                        IGNORE 1 LINES (`eq_site_limit`, `hu_site_limit`)", addslashes($path));
+
             $stmt = $this->getContainer()->get('doctrine')->getManager()->getConnection()->prepare($sql);
-            $stmt->execute(['eq_limit' => $r['eq_site_limit'], 'hu_limit' => $r['hu_site_limit']]);
-        }
+            $stmt->execute();
     }
+//    }
 
     public function getEntityManager()
     {
